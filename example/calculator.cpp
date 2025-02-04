444932c7c66c77c314b8dd0ca947a9644aab06fb
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-02-04 11:27:51 +0000

    minimum calculator example

--- example/calculator.cpp
+++ example/calculator.cpp
@@ -3,54 +3,59 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <calculator.h>
-#include <fstream>
-#include <init.capnp.h>
-#include <init.capnp.proxy.h> // IWYU pragma: keep
-#include <init.h>
-#include <iostream>
-#include <kj/async.h>
-#include <kj/common.h>
-#include <kj/memory.h>
-#include <memory>
-#include <mp/proxy-io.h>
-#include <printer.h>
-#include <stdexcept>
-#include <cstring>
-#include <utility>
+// #include <init.capnp.proxy.h>
+#include <init.h>  // for Init
+
+
+// #include <calculator.h>
+// #include <fstream>
+// #include <init.capnp.h>
+// #include <init.capnp.proxy.h> // IWYU pragma: keep
+// #include <init.h>
+// #include <iostream>
+// #include <kj/async.h>
+// #include <kj/common.h>
+// #include <kj/memory.h>
+// #include <memory>
+// #include <mp/proxy-io.h>
+// #include <printer.h>
+// #include <stdexcept>
+// #include <cstring>
+// #include <utility>
 
 class CalculatorImpl : public Calculator
 {
-public:
-    CalculatorImpl(std::unique_ptr<Printer> printer) : m_printer(std::move(printer)) {}
-    void solveEquation(const std::string& eqn) override { m_printer->print("Wow " + eqn + ", that's a tough one.\n"); }
-    std::unique_ptr<Printer> m_printer;
+// public:
+//     CalculatorImpl(std::unique_ptr<Printer> printer) : m_printer(std::move(printer)) {}
+//     void solveEquation(const std::string& eqn) override { m_printer->print("Wow " + eqn + ", that's a tough one.\n"); }
+//     std::unique_ptr<Printer> m_printer;
 };
 
 class InitImpl : public Init
 {
-public:
-    std::unique_ptr<Calculator> makeCalculator(std::unique_ptr<Printer> printer) override
-    {
-        return std::make_unique<CalculatorImpl>(std::move(printer));
-    }
+// public:
+//     std::unique_ptr<Calculator> makeCalculator(std::unique_ptr<Printer> printer) override
+//     {
+//         return std::make_unique<CalculatorImpl>(std::move(printer));
+//     }
 };
 
 void LogPrint(bool raise, const std::string& message)
 {
-    if (raise) throw std::runtime_error(message);
-    std::ofstream("debug.log", std::ios_base::app) << message << std::endl;
+    // if (raise) throw std::runtime_error(message);
+    // std::ofstream("debug.log", std::ios_base::app) << message << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        std::cout << "Usage: mpcalculator <fd>\n";
-        return 1;
-    }
-    mp::EventLoop loop("mpcalculator", LogPrint);
-    int fd = std::stoi(argv[1]);
-    std::unique_ptr<Init> init = std::make_unique<InitImpl>();
-    mp::ServeStream<InitInterface>(loop, fd, *init);
-    loop.loop();
+    // if (argc != 2) {
+    //     std::cout << "Usage: mpcalculator <fd>\n";
+    //     return 1;
+    // }
+    // mp::EventLoop loop("mpcalculator", LogPrint);
+    // int fd = std::stoi(argv[1]);
+    // std::unique_ptr<Init> init = std::make_unique<InitImpl>();
+    // mp::ServeStream<InitInterface>(loop, fd, *init);
+    // loop.loop();
     return 0;
 }

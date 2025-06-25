c8378c28c890fa6e75c475fd641573a42439becf
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2025-06-25 13:39:40 +0100

    Print tool versions

--- ci/scripts/ci.sh
+++ ci/scripts/ci.sh
@@ -5,6 +5,16 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 : "${CI_DIR:=build}"
 
+if command -v clang++ >/dev/null 2>&1; then
+  clang++ --version
+fi
+if command -v include-what-you-use >/dev/null 2>&1; then
+  include-what-you-use --version
+fi
+if command -v clang-tidy >/dev/null 2>&1; then
+  clang-tidy --version
+fi
+
 cmake -B "$CI_DIR" "${CMAKE_ARGS[@]+"${CMAKE_ARGS[@]}"}"
 cmake --build "$CI_DIR" -t all tests mpexamples -- "${BUILD_ARGS[@]+"${BUILD_ARGS[@]}"}"
 ctest --test-dir "$CI_DIR" --output-on-failure

401e0ce1d9c34ad375428190ee7c46ba6456c0e7
Sjors Provoost <sjors@sprovoost.nl>
2025-06-27 17:01:24 +0200

    ci: add copyright to bash scripts

--- ci/scripts/ci.sh
+++ ci/scripts/ci.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 export LC_ALL=C.UTF-8
 
 set -o errexit -o nounset -o pipefail -o xtrace

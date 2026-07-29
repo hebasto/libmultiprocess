37643fbb3582703b3ae3cb25f3b20549faf0a626
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2026-07-29 09:57:27 +0100

    [DEBUG] Add debug info

--- ci/configs/netbsd.bash
+++ ci/configs/netbsd.bash
@@ -1,5 +1,5 @@
 CI_DESC="CI config for NetBSD"
 CI_DIR=build-netbsd
-export CXXFLAGS="-Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter"
+export CXXFLAGS="-g -Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter"
 CMAKE_ARGS=(-G Ninja)
 BUILD_ARGS=(-v -k 0)
